2ced193278015dc81ff9e662f970c6d77438b5b5
naojitaniguchi <naoji.taniguchi@gmail.com>
2018-09-23 19:16:32 +0900

    first commit .

---
+++ Assets/PlayerChaseCamera.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerChaseCamera : MonoBehaviour {
+    public GameObject player;
+    public float xMax = 3.0f;
+    public float xMin = -3.0f;
+    public float yMax = 3.0f;
+    public float yMin = -3.0f;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        Vector3 distanceFromPlayer = player.transform.position - gameObject.transform.position;
+        if ( distanceFromPlayer.x > xMax ){
+            gameObject.transform.position = new Vector3(transform.position.x + distanceFromPlayer.x - xMax,
+                                                        transform.position.y, transform.position.z);
+        }
+        if (distanceFromPlayer.x < xMin)
+        {
+            gameObject.transform.position = new Vector3(transform.position.x + distanceFromPlayer.x - xMin,
+                                                        transform.position.y, transform.position.z);
+        }
+        if (distanceFromPlayer.y > yMax)
+        {
+            gameObject.transform.position = new Vector3(transform.position.x ,
+                                                        transform.position.y + distanceFromPlayer.y - yMax,
+                                                        transform.position.z);
+        }
+        if (distanceFromPlayer.y < yMin)
+        {
+            gameObject.transform.position = new Vector3(transform.position.x,
+                                                        transform.position.y + distanceFromPlayer.y - yMin,
+                                                        transform.position.z);
+        }
+    }
+}
---
+++ Assets/PlayerControl.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour {
+    public float moveSpeed;
+    Animator anim;
+    Rigidbody2D r2d;
+
+	// Use this for initialization
+	void Start () {
+        anim = gameObject.GetComponent<Animator>();
+        r2d = gameObject.GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        r2d.velocity = Vector2.zero;
+
+        if ( Input.GetKey(KeyCode.UpArrow)){
+            anim.SetBool("Idle", false);
+            anim.SetBool("Up", true);
+            r2d.velocity = Vector2.up * moveSpeed;
+        }
+        else{
+            anim.SetBool("Up", false);
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            anim.SetBool("Idle", false);
+            anim.SetBool("Down", true);
+            r2d.velocity = Vector2.down * moveSpeed;
+        }
+        else
+        {
+            anim.SetBool("Down", false);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            anim.SetBool("Idle", false);
+            anim.SetBool("Right", true);
+            r2d.velocity = Vector2.right * moveSpeed;
+        }
+        else
+        {
+            anim.SetBool("Right", false);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            anim.SetBool("Idle", false);
+            anim.SetBool("Left", true);
+            r2d.velocity = Vector2.left * moveSpeed;
+        }
+        else
+        {
+            anim.SetBool("Left", false);
+        }
+        if ( Input.GetKeyUp(KeyCode.UpArrow) || Input.GetKeyUp(KeyCode.DownArrow) ||
+            Input.GetKeyUp(KeyCode.LeftArrow) || Input.GetKeyUp(KeyCode.RightArrow) ){
+            anim.SetBool("Idle", true);
+        }
+        
+    }
+}
