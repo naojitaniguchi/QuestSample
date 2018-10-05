8cf7ad93033b4070ee0ed1602845d06129618b3f
naojitaniguchi <naoji.taniguchi@gmail.com>
2018-10-05 13:44:20 +0900

    Random attack script Enemy.cs added .

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+    public bool alive = true;
+    public string[] animType;
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine("Attack");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    // コルーチン  
+    private IEnumerator Attack()
+    {
+        // コルーチンの処理  
+        while( alive ){
+            float waitTime = Random.Range(1.0f, 3.0f);
+            yield return new WaitForSeconds(waitTime);
+
+            Debug.Log("Attack");
+
+            int animIndex = Random.Range(0, animType.Length);
+            Debug.Log(animType[animIndex]);
+
+            // anim.SetBool(animType[animIndex], true);
+
+        }
+    }
+}
+
+
+// http://developer.wonderpla.net/entry/blog/engineer/Unity_Co-routine/
